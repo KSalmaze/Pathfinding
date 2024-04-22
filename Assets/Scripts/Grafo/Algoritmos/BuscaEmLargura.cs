6a0281686fa74653da85e732c7dc104fca98a456
KSalmaze <pedrohsalmaze@gmail.com>
2024-04-22 18:47:14 -0300

    Busca em largura comentada

--- Assets/Scripts/Grafo/Algoritmos/BuscaEmLargura.cs
+++ Assets/Scripts/Grafo/Algoritmos/BuscaEmLargura.cs
@@ -6,60 +6,94 @@ public class BuscaEmLargura : AlgoritmoDeBusca
 {
     private Grafo _grafo;
 
+    // Corotina para a execução do algoritmo de Busca em Largura
     override public IEnumerator Comecar(Grafo grafo, ColorConsts cc)
     {
-        _grafo = grafo;
-        colorConsts = cc;
+        _grafo = grafo; // Armazena a referência do grafo.
+        colorConsts = cc; // Configura as constantes de cores.
+
+        // Dicionário para amazenar o caminho para cada nó
         Dictionary<Node, Node> nosAnteriores = new Dictionary<Node, Node>();
+        // Fila necessária para a execução do método
         Queue<Node> fila = new Queue<Node>();
+
+        // Inicializa a fila com o primeiro nó
         fila.Enqueue(grafo.PosicaoInicial());
 
+        // Enquanto houver algum nó na fila, continua a busca
         while (fila.Count > 0)
         {
+            // Define o nó atual como o primeiro da fila
             Node noAtual = fila.Dequeue();
+
+
+            // Se o nó atual já tiver sido descoberto, ele é marcado como visitado
             if (noAtual.status == colorConsts.DESCOBERTO)
             {
                 noAtual.MudarStatus(colorConsts.VISITADO);
             }
 
+            // Para cada nó vizinho ao nó atual
             foreach (Node no in grafo.NosVizinhos(noAtual.position))
             {
                 Debug.Log("Descobrindo o nó " + no.position);
+
+                // Verifica o status do vizinho.
                 Color status = no.status;
 
+                // Se o vizinho for o jogador
                 if (status == colorConsts.PLAYER)
                 {
                     Debug.Log("Encontrou o jogador");
+                    // Marca o nó atual como o nó anterior ao jogador
                     nosAnteriores.Add(no, noAtual);
+
+                    // Chama a função de Player encontrado
                     PlayerEncontrado(nosAnteriores, noAtual);
+
+                    // Finaliza a busca
                     yield break;
                 }
 
+                // Se o nó ainda não tiver sido descoberto, marca como descoberto
                 if (status == colorConsts.NAO_DESCOBERTO)
                 {
                     nosAnteriores.Add(no, noAtual);
                     no.MudarStatus(grafo.colorConsts.DESCOBERTO);
+
+                    // Adiciona na fila
                     fila.Enqueue(no);
                 }
+
+                // Passa para o próximo frame para atualizar a tela
                 yield return null;
             }
+
+            // Passa para o próximo frame para atualizar a tela
             yield return null;
         }
 
         grafo.GerarEstatisticas();
     }
 
+
+    // Método privado para tratar o encontro com o jogador.
     private void PlayerEncontrado(Dictionary<Node, Node> nosAnteriores, Node ultimoNo)
     {
+        // Lista para armazenar o caminho encontrado.
         List<Node> caminho = new List<Node>();
+        // Começa pelo último nó (onde o jogador foi encontrado).
         Node noAtual = ultimoNo;
 
+        // Retrocede pelo caminho usando o dicionário até chegar ao início.
         do
         {
             caminho.Add(noAtual);
             noAtual = nosAnteriores[noAtual];
         } while (noAtual.position != _grafo.PosicaoInimigo);
+        // O loop termina quando chega à posição do inimigo.
 
+        // Após construir o caminho, mostra-o no jogo.
         _grafo.MostrarCaminho(caminho);
     }
 }
--- Assets/Scripts/Grafo/Node.cs
+++ Assets/Scripts/Grafo/Node.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    public Color status;
-    public (int x, int y) position;
-    private Grafo grafo;
-    private UnityEngine.UI.Image image;
+    public Color status; // Cor que define o status do nó
+    public (int x, int y) position; // A posição do nó na malha
+    private Grafo grafo; // Referência ao grafo
+    private UnityEngine.UI.Image image; // A sprite do nó
 
+    // Função da Unity que é chamada assim que o nó é criado
     public void Awake()
     {
+        // Inicializa as váriaveis de grafo e imagem
         grafo = GameObject.Find("GameManager").GetComponent<Grafo>();
         image = gameObject.GetComponent<UnityEngine.UI.Image>();
     }
 
+    // Função chama na criação do nó, recebe qual deve ser a sua posição no grafo
+    // e o status inicial
     public void InicializarNo((int x,int y) pos, Color statusInicial)
     {
         position = pos;
@@ -22,11 +26,13 @@ public class Node : MonoBehaviour
         SeColocarNoGrafo();
     }
 
+    // Assim que o nó é criado ele chama essa função pra ser colocado no grafo
     public void SeColocarNoGrafo()
     {
         grafo.nos[position.y][position.x] = this;
     }
 
+    // Função para facilitar a mundança de status de um nó
     public void MudarStatus(Color cor)
     {
         image.color = cor;
